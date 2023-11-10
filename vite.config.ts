35b1c0585cc927679e24556342e54ee24ad626e5
Sohailsaifi <sohailsaifi65@gmail.com>
2023-11-10 21:50:37 +0530

    path resolver added to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {
-  resolve
-} from "path";
-
-const aliases = {
-  '@crema': 'src/@crema',
-  'core': 'src/core',
-  'assets': 'src/assets',
-  '@hook': 'src/@hook',
-  'components': 'src/components',
-  'features': 'src/features',
-  'guards': 'src/guards',
-  'pages': 'src/pages',
-  'types': 'src/types',
-};
-
-const resolvedAliases = Object.fromEntries(
-  Object.entries(aliases).map(([key, value]) => [key, resolve(__dirname, value)]),
-);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-        external: [
-            "react", // ignore react stuff
-            "react-dom",
-        ],
-    }
-  },
-  resolve: {
-      alias: {
-          ...resolvedAliases,
-          './runtimeConfig': './runtimeConfig.browser',
-          'jss-plugin-{}': 'jss-plugin-global'
-      },
-  },
+  build:{
+    outDir: "dist"
+  }
 })
